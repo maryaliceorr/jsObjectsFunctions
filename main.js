fa333b852798bea715b77937fbd1fd7276d40522
Mary Alice Orr <m.a.orr17@gmail.com>
2018-03-03 10:10:09 -0600

    added petnames

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
 console.log("hello");
+
+var favAnimal = {
+    name: "Mary",
+    animal: "dog"
+};
+
+console.log("favAnimal", favAnimal.animal);
+console.log("favAnimal", favAnimal["animal"]);
+
+var instructorPets = [{
+    instructorName: "Zoe",
+    petName: "Biff",
+    animalType: "evil cat",
+    isFavorite: false
+},
+{
+    instructorName: "Callan",
+    petName: "Seymour",
+    animalType: "one eyed dog",
+    isFavorite: true
+},
+{
+    instructorName: "Lauren",
+    petName: "Frankie",
+    animalType: "sweet dog",
+    isFavorite: true
+},
+{
+    instructorName: "Callan",
+    petName: "Wiley",
+    animalType: "destructive dog",
+    isFavorite: false
+
+}];
+
+console.log("instructorPets", instructorPets);
+
+// div wtih id="pet-names"
+// loop through instructorPets and print petName as h1 tag to that div
+
+var animalNames = document.getElementById("animal-name-holder");
+
+for (var i=0; i<instructorPets.length; i++) {
+    animalNames.innerHTML += "<h1>" + instructorPets[i].petName + "</h1>";
+}
+
+console.log("animal names", animalNames);
